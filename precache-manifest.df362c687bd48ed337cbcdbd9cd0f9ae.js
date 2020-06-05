2b1f3a467b239cf9abf4a1375c19dae7884602c3
Jakob <jakob.thomasson@jayway.com>
2020-06-05 15:24:08 +0200

    Updates

---
+++ precache-manifest.df362c687bd48ed337cbcdbd9cd0f9ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff17b222fd52b1912828e6daa83df79",
+    "url": "/home-exercise-jakob/index.html"
+  },
+  {
+    "revision": "3c4074452feeddbf79db",
+    "url": "/home-exercise-jakob/static/css/main.aa914634.chunk.css"
+  },
+  {
+    "revision": "210fa044f91ab469a87e",
+    "url": "/home-exercise-jakob/static/js/2.2d51fd6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home-exercise-jakob/static/js/2.2d51fd6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4074452feeddbf79db",
+    "url": "/home-exercise-jakob/static/js/main.f4cc93c3.chunk.js"
+  },
+  {
+    "revision": "d36cab1d557fb335c952",
+    "url": "/home-exercise-jakob/static/js/runtime-main.a560edfc.js"
+  }
+]);
